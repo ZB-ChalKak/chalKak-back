97961e3361c87181c59cd93a9083b9d80fbc76f1
jyjang <jinyngg@gmail.com>
2023-08-14 23:32:02 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/com/btb/chalKak/ChalKakApplication.java
@@ -0,0 +1,13 @@
+package com.btb.chalKak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalKakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalKakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btb/chalKak/ChalKakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btb.chalKak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalKakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
