663eaaa18d31bee11e9f0b329ca457fb7e968d84
myu7769 <myung7769@gamil.com>
2023-08-16 06:36:09 +0900

    [#6] feat : user,post Entity/Repository 생성

---
+++ src/main/java/com/btb/chalKak/domain/post/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.btb.chalKak.domain.post.repository;
+
+import com.btb.chalKak.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
