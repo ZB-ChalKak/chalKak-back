c092fea978d9757812e753e5385b47132521a79b
myung7769 <myung7769@gmail.com>
2023-08-17 01:08:48 +0900

    [#3] feat : Oauth2 service 클래스 생성

---
+++ src/main/java/com/btb/chalKak/global/oauth2/service/CustomOAuth2UserService.java
@@ -0,0 +1,8 @@
+package com.btb.chalKak.global.oauth2.service;
+
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public interface CustomOAuth2UserService extends OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+}
