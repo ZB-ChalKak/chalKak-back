dfc74cd3bca209ecc199521debc479f5a6bbec20
myung7769 <myung7769@gmail.com>
2023-08-17 01:23:43 +0900

    [#3] fix : Member 클래스 변경에 따른 코드 수정

--- src/main/java/com/btb/chalKak/domain/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.btb.chalKak.domain.user.repository;
-
-import com.btb.chalKak.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
