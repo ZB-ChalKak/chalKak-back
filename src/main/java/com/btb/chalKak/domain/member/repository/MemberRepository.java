5a607c67b9c7d8f34e78fc72fb9351f0e9a0c5f6
myung7769 <myung7769@gmail.com>
2023-08-17 02:01:21 +0900

    [#3] fix : member 클래스 변경에 따른 수정 2
    MemberRepository 함수, member update 함수 추가

--- src/main/java/com/btb/chalKak/domain/member/repository/MemberRepository.java
+++ src/main/java/com/btb/chalKak/domain/member/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package com.btb.chalKak.domain.member.repository;
 import com.btb.chalKak.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+
+    Optional<Member> findByEmail(String email);
 }
