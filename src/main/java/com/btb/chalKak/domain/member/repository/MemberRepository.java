abf601a58b724a0ba9b0fe75ff33f05eea0f287e
jyjang <jinyngg@gmail.com>
2023-08-17 19:39:00 +0900

    [#9] feat : Member 테이블 수정
    1. 칼럼 명시

--- src/main/java/com/btb/chalKak/domain/member/repository/MemberRepository.java
+++ src/main/java/com/btb/chalKak/domain/member/repository/MemberRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-
     Optional<Member> findByEmail(String email);
+
     Optional<Member> findById(Long id);
 }
