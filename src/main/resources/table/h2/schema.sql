5cd64d8b6943b1de8def647af06dfbb584a0b8e9
jyjang <jinyngg@gmail.com>
2023-08-17 22:25:44 +0900

    [#23] feat : 더미데이터 추가 및 스키마 작성 추가

--- src/main/resources/table/h2/schema.sql
+++ src/main/resources/table/h2/schema.sql
@@ -1,15 +1,31 @@
 -- 테스트를 위한 DDL 작성
-DROP TABLE IF EXISTS static_tag;
+DROP TABLE IF EXISTS style_tag;
+DROP TABLE IF EXISTS hash_tag;
 DROP TABLE IF EXISTS member;
-DROP TABLE IF EXISTS member_static_tag;
+DROP TABLE IF EXISTS member_style_tag;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS post_style_tag;
+DROP TABLE IF EXISTS post_hash_tag;
 
 -- 정적 태그 (스타일 태그)
-CREATE TABLE static_tag
+CREATE TABLE style_tag
 (
-    static_tag_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    category      VARCHAR(20)         NOT NULL,
-    keyword       VARCHAR(100) UNIQUE NOT NULL,
-    count         BIGINT
+    style_tag_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    category     VARCHAR(20)         NOT NULL,
+    keyword      VARCHAR(100) UNIQUE NOT NULL,
+    count        BIGINT,
+    created_at     TIMESTAMP           NOT NULL DEFAULT NOW(),
+    updated_at     TIMESTAMP           NOT NULL DEFAULT NOW()
+);
+
+-- 동적 태그 (해시 태그)
+CREATE TABLE hash_tag
+(
+    hash_tag_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    keyword     VARCHAR(100) UNIQUE NOT NULL,
+    count       BIGINT,
+    created_at     TIMESTAMP           NOT NULL DEFAULT NOW(),
+    updated_at     TIMESTAMP           NOT NULL DEFAULT NOW()
 );
 
 -- 멤버 정보
@@ -31,13 +47,46 @@ CREATE TABLE member
     updated_at     TIMESTAMP           NOT NULL DEFAULT NOW()
 );
 
--- 멤버와 정적 태그 조인 테이블
-CREATE TABLE member_static_tag
+-- 멤버와 스타일 태그 조인 테이블
+CREATE TABLE member_style_tag
 (
-    member_id     BIGINT NOT NULL,
-    static_tag_id BIGINT NOT NULL,
-    PRIMARY KEY (member_id, static_tag_id),
+    member_id    BIGINT NOT NULL,
+    style_tag_id BIGINT NOT NULL,
+    PRIMARY KEY (member_id, style_tag_id),
     FOREIGN KEY (member_id) REFERENCES member (member_id),
-    FOREIGN KEY (static_tag_id) REFERENCES static_tag (static_tag_id)
+    FOREIGN KEY (style_tag_id) REFERENCES style_tag (style_tag_id)
 );
 
+-- 포스트 정보
+CREATE TABLE post
+(
+    post_id    BIGINT PRIMARY KEY AUTO_INCREMENT,
+    content    TEXT,
+    hit_count  BIGINT,
+    like_count BIGINT,
+    status     VARCHAR(20) NOT NULL,
+    member_id  BIGINT,
+    created_at     TIMESTAMP           NOT NULL DEFAULT NOW(),
+    updated_at     TIMESTAMP           NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (member_id) REFERENCES member (member_id)
+);
+
+-- 포스트와 스타일 태그 조인 테이블
+CREATE TABLE post_style_tag
+(
+    post_id      BIGINT NOT NULL,
+    style_tag_id BIGINT NOT NULL,
+    PRIMARY KEY (post_id, style_tag_id),
+    FOREIGN KEY (post_id) REFERENCES post (post_id),
+    FOREIGN KEY (style_tag_id) REFERENCES style_tag (style_tag_id)
+);
+
+-- 포스트와 해시 태그 조인 테이블
+CREATE TABLE post_hash_tag
+(
+    post_id     BIGINT NOT NULL,
+    hash_tag_id BIGINT NOT NULL,
+    PRIMARY KEY (post_id, hash_tag_id),
+    FOREIGN KEY (post_id) REFERENCES post (post_id),
+    FOREIGN KEY (hash_tag_id) REFERENCES hash_tag (hash_tag_id)
+);
