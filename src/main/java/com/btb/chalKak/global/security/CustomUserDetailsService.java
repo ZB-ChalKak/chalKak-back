61d03444a3e8f6b449d7ba40a930432c26ce6121
JoHyungJun <jhj8371@naver.com>
2023-08-17 17:41:33 +0900

    [#9] fix : Exception 수정 및 권한 별 return 값 수정

--- src/main/java/com/btb/chalKak/global/security/CustomUserDetailsService.java
+++ src/main/java/com/btb/chalKak/global/security/CustomUserDetailsService.java
@@ -17,7 +17,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Member member = getMember(email);
