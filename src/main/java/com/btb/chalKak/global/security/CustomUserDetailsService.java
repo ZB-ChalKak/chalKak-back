8ad958a884842426aad3f7f27558339296f1dab5
jyjang <jinyngg@gmail.com>
2023-08-17 19:40:14 +0900

    [#9] fix : username 변경
    1. username은 email로 변경하여 사용하기 때문에 해당 내용으로 수정

--- src/main/java/com/btb/chalKak/global/security/CustomUserDetailsService.java
+++ src/main/java/com/btb/chalKak/global/security/CustomUserDetailsService.java
@@ -3,13 +3,10 @@ package com.btb.chalKak.global.security;
 import com.btb.chalKak.domain.member.entity.Member;
 import com.btb.chalKak.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,15 +16,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Member member = getMember(email);
-
+        Member member = getMemberByEmail(email);
         return new CustomUserDetails(member);
     }
 
-    private Member getMember(String email) {
-        Member member = memberRepository.findById(Long.parseLong(email))
+    private Member getMemberByEmail(String email) {
+        return memberRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("회원 정보를 불러오는데 실패했습니다."));
-
-        return member;
     }
 }
