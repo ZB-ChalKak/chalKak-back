c9fb20861252315fae7db908db713b73362ada69
jyjang <jinyngg@gmail.com>
2023-08-18 18:03:54 +0900

    feat : 해쉬태그 Entity 변경
    1. count 필드 제거
    2. 키워드로 HashTag 찾기 추가

---
+++ src/main/java/com/btb/chalKak/domain/hashTag/repository/HashTagRepository.java
@@ -0,0 +1,11 @@
+package com.btb.chalKak.domain.hashTag.repository;
+
+import com.btb.chalKak.domain.hashTag.entity.HashTag;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashTagRepository extends JpaRepository<HashTag, Long> {
+
+    Optional<HashTag> findByKeyword(String keyword);
+
+}
