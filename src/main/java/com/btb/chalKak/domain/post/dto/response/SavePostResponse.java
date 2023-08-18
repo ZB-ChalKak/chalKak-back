1bbfd13484e8813a4b0633465892d2aec0982171
jyjang <jinyngg@gmail.com>
2023-08-18 18:05:24 +0900

    [#28] feat : 게시물 작성을 위한 Req/Res DTO 작성
    1. 이미지 관련 작성 필요

---
+++ src/main/java/com/btb/chalKak/domain/post/dto/response/SavePostResponse.java
@@ -0,0 +1,12 @@
+package com.btb.chalKak.domain.post.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SavePostResponse {
+
+    private Long postId;
+
+}
