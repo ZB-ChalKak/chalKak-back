ff846790930a03db69365e4ddb0f4f96c2158428
myu7769 <myung7769@gamil.com>
2023-08-24 15:36:36 +0900

    [#12] fix : weather return time 수정

--- src/main/java/com/btb/chalKak/domain/weather/scheduler/WeatherScheduler.java
+++ src/main/java/com/btb/chalKak/domain/weather/scheduler/WeatherScheduler.java
@@ -1,6 +1,8 @@
 package com.btb.chalKak.domain.weather.scheduler;
 
+import com.btb.chalKak.domain.weather.dto.WeatherDto;
 import com.btb.chalKak.domain.weather.repository.WeatherRepository;
+import com.btb.chalKak.domain.weather.service.WeatherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -12,14 +14,18 @@ public class WeatherScheduler {
 
     private final WeatherRepository weatherRepository;
 
+    private final WeatherService weatherService;
+
     @Transactional
     @Scheduled(cron = "${scheduler.weather.cron}")
     public void createNewWeatherForMember() {
 
 
+        String lat = "";
+        String lon = "";
         // 1. 시도 중요 지점의 위도와 경도를 가져온다.
         // 2. weather 서비스로 5일치 3시간 간격의 weather data를 가져온다.
-
+        WeatherDto weatherDto = weatherService.getWeatherFromApi(lat,lon);
         // 3. 1일치의 칼럼으로 만든다.
         // 3-1. 평균 기온 , 최고 기온, 최저 기온,
         // 4. 5일치의 칼럼을 만들어 repository 저장한다.
--- src/main/java/com/btb/chalKak/domain/weather/service/WeatherService.java
+++ src/main/java/com/btb/chalKak/domain/weather/service/WeatherService.java
@@ -5,7 +5,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
@@ -64,7 +66,7 @@ public class WeatherService {
     // 받아온 날씨 json 파싱
     Map<String, Object> parsedWeather = parseWeather(weatherDate);
     WeatherDto dateWeather = WeatherDto.builder()
-        .date(LocalDate.now())
+        .date(LocalDate.ofInstant(Instant.ofEpochSecond((Long) parsedWeather.get("date")), ZoneId.systemDefault()))
         .weather(parsedWeather.get("main").toString())
         .temperature((Double) parsedWeather.get("temp"))
         .icon(parsedWeather.get("icon").toString())
@@ -81,16 +83,22 @@ public class WeatherService {
 
       jsonObject = (JSONObject) jsonParser.parse(jsonString);
 
-
     } catch (ParseException e) {
       throw new RuntimeException(e);
     }
     Map<String, Object> resultMap = new HashMap<>();
     JSONArray weatherList = (JSONArray)jsonObject.get("list");
 
+    log.info("weather size" + weatherList.size());
+
     JSONObject weatherData = (JSONObject) weatherList.get(0);
     JSONObject mainData = (JSONObject) weatherData.get("main");
+    Long dtData = (Long) weatherData.get("dt");
+    String dataData = (String) weatherData.get("dt_txt");
     log.info(mainData.toJSONString());
+    log.info("dataData" + dataData);
+    log.info("dataData " + dtData);
+    resultMap.put("date", dtData );
     resultMap.put("temp", mainData.get("temp"));
 
     JSONArray weatherArray = (JSONArray) weatherData.get("weather");
