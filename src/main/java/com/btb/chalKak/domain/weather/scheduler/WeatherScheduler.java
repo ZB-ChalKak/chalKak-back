54597140696fd41baa67fd3d9776be61ceab0eda
myu7769 <myung7769@gamil.com>
2023-08-24 16:00:29 +0900

    [#12] fix : List 형식으로 weather data 반환

--- src/main/java/com/btb/chalKak/domain/weather/scheduler/WeatherScheduler.java
+++ src/main/java/com/btb/chalKak/domain/weather/scheduler/WeatherScheduler.java
@@ -1,8 +1,10 @@
 package com.btb.chalKak.domain.weather.scheduler;
 
 import com.btb.chalKak.domain.weather.dto.WeatherDto;
+import com.btb.chalKak.domain.weather.entity.Weather;
 import com.btb.chalKak.domain.weather.repository.WeatherRepository;
 import com.btb.chalKak.domain.weather.service.WeatherService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -25,14 +27,20 @@ public class WeatherScheduler {
         String lon = "";
         // 1. 시도 중요 지점의 위도와 경도를 가져온다.
         // 2. weather 서비스로 5일치 3시간 간격의 weather data를 가져온다.
-        WeatherDto weatherDto = weatherService.getWeatherFromApi(lat,lon);
-        // 3. 1일치의 칼럼으로 만든다.
+        List<WeatherDto> weatherDto = weatherService.getWeatherFromApi(lat,lon);
+        // 3. 1일치를 하나의 칼럼으로 만든다.
         // 3-1. 평균 기온 , 최고 기온, 최저 기온,
-        // 4. 5일치의 칼럼을 만들어 repository 저장한다.
+        convertDtosToWeathers(weatherDto);
+
+        // 4. 5일치의 칼럼을 만들어 repository 저장한다. -> 하루치만 있어도 충분할지도? 아니면 5일까지 추천해줄 수 있도록하는 것도 괜찮을듯
 
     }
 
-    private long getPostIdByRedisKey(String postViewCountKey) {
-        return Long.parseLong(postViewCountKey.split("::")[1]);
+    private List<Weather> convertDtosToWeathers(List<WeatherDto> weatherDtos) {
+
+
+
+
+        return null;
     }
 }
