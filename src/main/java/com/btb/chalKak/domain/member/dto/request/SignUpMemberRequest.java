eab85881dcf991306a71d9514067ca7be550dca2
jyjang <jinyngg@gmail.com>
2023-08-24 15:42:02 +0900

    [#41] feat : 회원가입 요청 dto 수정
    1. Gender -> 설정되어 있는 Enum 으로 수정
    2. 프라이버시 필드 추가
    3. 키워드 값 Id로 받아오는 것으로 수정(FE에서 상수로 관리할 경우, String으로 변경 필요)

--- src/main/java/com/btb/chalKak/domain/member/dto/request/SignUpMemberRequest.java
+++ src/main/java/com/btb/chalKak/domain/member/dto/request/SignUpMemberRequest.java
@@ -1,19 +1,44 @@
 package com.btb.chalKak.domain.member.dto.request;
 
+import com.btb.chalKak.domain.member.type.Gender;
+import java.util.List;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
 
 @Getter
 @Builder
 public class SignUpMemberRequest {
 
+    @NotBlank(message = "이메일이 누락되었습니다.")
+    @Email(message = "이메일 주소 형식이 잘못되었습니다.")
     private String email;
+
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?!.*\\s).+$"
+            , message = "비밀번호는 영어와 숫자를 혼용해야 하며 공백은 사용할 수 없습니다.")
+    @Size(min = 8, max = 16, message = "비밀번호는 최소 8글자 이상 최대 16글자 이하로 작성해야 합니다.")
     private String password;
+
+    @NotBlank(message = "닉네임이 누락되었습니다.")
     private String nickname;
-    private String gender;
+
+    @NotBlank(message = "성별이 누락되었습니다.")
+    private Gender gender;
+
+    @NotNull(message = "키가 누락되었습니다.")
     private Double height;
+    @NotNull(message = "체중이 누락되었습니다.")
     private Double weight;
-    private List<String> keywords;
+
+    @NotNull(message = "키 프라이버시 설정이 누락되었습니다.")
+    private boolean privacyHeight;
+    @NotNull(message = "체중 프라이버시 설정이 누락되었습니다.")
+    private boolean privacyWeight;
+
+    @NotNull(message = "키워드가 누락되었습니다.")
+    private List<Long> styleTags;
 }
