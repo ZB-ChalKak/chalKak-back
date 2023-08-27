e0a41ba4ed6e665d5ba6ab1ec601d2abfb549ff6
myung7769 <myung7769@gmail.com>
2023-08-27 16:21:06 +0900

    [#47] feat : Like 기능 구현

--- src/main/java/com/btb/chalKak/common/response/type/SuccessCode.java
+++ src/main/java/com/btb/chalKak/common/response/type/SuccessCode.java
@@ -17,6 +17,18 @@ public enum SuccessCode {
     SUCCESS_REISSUE("토큰 재발급 성공"),
     SUCCESS_LOAD_WEATHER("날씨 조회에 성공했습니다."),
 
+    // 댓글
+
+    SUCCESS_SAVE_COMMENT("댓글 등록에 성공했습니다.")    ,
+    SUCCESS_LOAD_COMMENT("댓글 조회에 성공했습니다.") ,
+    SUCCESS_MODIFY_COMMENT("댓글 수정 성공했습니다.")  ,
+    SUCCESS_DELETE_COMMENT("댓글 삭제에 성공했습니다.")    ,
+
+    // 좋아요
+    SUCCESS_LIKE("좋아요 등록에 성공했습니다."),
+    SUCCESS_UNLIKE("좋아요 삭제에 성공했습니다."),
+
+
     ;
 
     private final String message;
