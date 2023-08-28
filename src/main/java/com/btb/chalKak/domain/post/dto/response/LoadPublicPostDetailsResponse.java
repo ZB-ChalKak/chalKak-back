9969ca24f9d56c9d2f516a73a29e6b63d0c46d93
jyjang <jinyngg@gmail.com>
2023-08-28 13:32:18 +0900

    [#54] feat : 글 작성시 체형 공개 여부 추가

--- src/main/java/com/btb/chalKak/domain/post/dto/response/LoadPublicPostDetailsResponse.java
+++ src/main/java/com/btb/chalKak/domain/post/dto/response/LoadPublicPostDetailsResponse.java
@@ -1,7 +1,11 @@
 package com.btb.chalKak.domain.post.dto.response;
 
+import com.btb.chalKak.domain.hashTag.entity.HashTag;
 import com.btb.chalKak.domain.member.dto.Writer;
 import com.btb.chalKak.domain.post.entity.Post;
+import com.btb.chalKak.domain.styleTag.entity.StyleTag;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,17 +15,36 @@ public class LoadPublicPostDetailsResponse {
 
     private Long id;
     private String content;
+    private String location;
+
     private Long viewCount;
     private Long likeCount;
+
+    private boolean privacyHeight;
+    private boolean privacyWeight;
+
+    private List<String> styleTags;
+    private List<String> hashTags;
+
     private Writer writer;
 
     public static LoadPublicPostDetailsResponse fromEntity(Post post) {
         return LoadPublicPostDetailsResponse.builder()
                 .id(post.getId())
                 .content(post.getContent())
+                .location(post.getLocation())
                 .viewCount(post.getViewCount())
                 .likeCount(post.getLikeCount())
+                .privacyHeight(post.isPrivacyHeight())
+                .privacyWeight(post.isPrivacyWeight())
+                .styleTags(post.getStyleTags().stream()
+                        .map(StyleTag::getKeyword)
+                        .collect(Collectors.toList()))
+                .hashTags(post.getHashTags().stream()
+                        .map(HashTag::getKeyword)
+                        .collect(Collectors.toList()))
                 .writer(Writer.fromEntity(post.getWriter()))
                 .build();
     }
+
 }
--- src/main/java/com/btb/chalKak/domain/post/dto/response/LoadPublicPostsResponse.java
+++ src/main/java/com/btb/chalKak/domain/post/dto/response/LoadPublicPostsResponse.java
@@ -32,4 +32,5 @@ public class LoadPublicPostsResponse {
                 .posts(loadPublicPostDetailsResponses)
                 .build();
     }
+
 }
