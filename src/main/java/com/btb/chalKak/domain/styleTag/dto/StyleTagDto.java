88bac3e1ab58f5ac29c34f6cf173acfd9c657a89
jyjang <jinyngg@gmail.com>
2023-08-29 21:31:01 +0900

    [#70] feat : 이미지 칼럼 추가

--- src/main/java/com/btb/chalKak/domain/styleTag/dto/StyleTagDto.java
+++ src/main/java/com/btb/chalKak/domain/styleTag/dto/StyleTagDto.java
@@ -1,5 +1,6 @@
 package com.btb.chalKak.domain.styleTag.dto;
 
+import com.btb.chalKak.domain.styleTag.entity.StyleTag;
 import com.btb.chalKak.domain.styleTag.type.StyleCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +15,16 @@ public class StyleTagDto {
 
     private Long id;
     private StyleCategory category;
+    private String keywordImg;
     private String keyword;
+
+    public static StyleTagDto fromEntity(StyleTag styleTag) {
+        return StyleTagDto.builder()
+                .id(styleTag.getId())
+                .category(styleTag.getCategory())
+                .keywordImg(styleTag.getKeywordImg())
+                .keyword(styleTag.getKeyword())
+                .build();
+    }
 
 }
