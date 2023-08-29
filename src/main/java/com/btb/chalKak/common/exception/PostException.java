251530e0a932d577194b0fc00a2199bebdb4b98b
jyjang <jinyngg@gmail.com>
2023-08-29 17:04:18 +0900

    [#68] docs : Exception 띄어쓰기 및 들여쓰기 수정

--- src/main/java/com/btb/chalKak/common/exception/PostException.java
+++ src/main/java/com/btb/chalKak/common/exception/PostException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class PostException extends RuntimeException{
+public class PostException extends RuntimeException {
 
     private final HttpStatus httpStatus;
     private final String message;
