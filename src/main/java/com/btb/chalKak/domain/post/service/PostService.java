cc929d5127656168262b2a60c01aba74e58caeb3
jyjang <jinyngg@gmail.com>
2023-08-30 00:58:24 +0900

    [#73] feat : 게시글 수정 로직 추가

--- src/main/java/com/btb/chalKak/domain/post/service/PostService.java
+++ src/main/java/com/btb/chalKak/domain/post/service/PostService.java
@@ -1,5 +1,6 @@
 package com.btb.chalKak.domain.post.service;
 
+import com.btb.chalKak.domain.post.dto.request.EditPostRequest;
 import com.btb.chalKak.domain.post.dto.request.WritePostRequest;
 import com.btb.chalKak.domain.post.entity.Post;
 import org.springframework.data.domain.Page;
@@ -9,6 +10,8 @@ import org.springframework.security.core.Authentication;
 public interface PostService {
     
     Post write(Authentication authentication, WritePostRequest request);
+
+    Post edit(Authentication authentication, Long postId, EditPostRequest request);
 
     Page<Post> loadPublicPosts(Pageable pageable);
 
