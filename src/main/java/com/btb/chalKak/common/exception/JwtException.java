5353fcf3a6df84168bbc0b9fa923a8f1bad3c32f
jyjang <jinyngg@gmail.com>
2023-08-29 17:04:48 +0900

    [#68] feat : JWT Exception 추가

---
+++ src/main/java/com/btb/chalKak/common/exception/JwtException.java
@@ -0,0 +1,19 @@
+package com.btb.chalKak.common.exception;
+
+import com.btb.chalKak.common.response.type.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class JwtException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+    private final String message;
+    private final String code;
+
+    public JwtException(ErrorCode errorCode) {
+        this.httpStatus = errorCode.getHttpStatus();
+        this.message = errorCode.getMessage();
+        this.code = errorCode.name();
+    }
+}
