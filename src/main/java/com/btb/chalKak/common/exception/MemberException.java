2e33774500b5689bb7ea1808319f70a43dfa6f0b
jyjang <jinyngg@gmail.com>
2023-08-29 17:04:18 +0900

    [#68] docs : Exception 띄어쓰기 및 들여쓰기 수정

--- src/main/java/com/btb/chalKak/common/exception/MemberException.java
+++ src/main/java/com/btb/chalKak/common/exception/MemberException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class MemberException extends RuntimeException{
+public class MemberException extends RuntimeException {
 
     private final HttpStatus httpStatus;
     private final String message;
