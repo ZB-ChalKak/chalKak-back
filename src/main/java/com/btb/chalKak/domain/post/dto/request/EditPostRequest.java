0bc6640e872596bbecbbf64ae9af702ba9f203ff
jyjang <jinyngg@gmail.com>
2023-08-30 00:33:10 +0900

    [#73] feat : 게시글 수정 Req/Res 추가

---
+++ src/main/java/com/btb/chalKak/domain/post/dto/request/EditPostRequest.java
@@ -0,0 +1,23 @@
+package com.btb.chalKak.domain.post.dto.request;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class EditPostRequest {
+
+    private String content;
+    private String location;
+
+    private List<Long> styleTags;
+    private List<String> hashTags;
+
+    private boolean privacyHeight;
+    private boolean privacyWeight;
+
+
+    // TODO 이미지
+
+}
