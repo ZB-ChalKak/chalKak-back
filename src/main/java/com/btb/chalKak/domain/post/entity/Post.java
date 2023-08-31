a8ba719fefb58489d8cbf0837b401f5ed060567e
jyjang <jinyngg@gmail.com>
2023-08-31 20:28:28 +0900

    [#89] feat : 칼럼에 포함되지 않는 팔로잉 여부, 좋아요 여부 추가

--- src/main/java/com/btb/chalKak/domain/post/entity/Post.java
+++ src/main/java/com/btb/chalKak/domain/post/entity/Post.java
@@ -24,6 +24,7 @@ import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -54,14 +55,15 @@ public class Post extends BaseTimeEntity {
     @Column(name = "like_count", nullable = false)
     private Long likeCount = 0L;
 
-    @Column(name = "privacy_height")
+    @Column(name = "privacy_height", nullable = false)
     private boolean privacyHeight;
 
-    @Column(name = "privacy_weight")
+    @Column(name = "privacy_weight", nullable = false)
     private boolean privacyWeight;
 
+    @Builder.Default
     @Column(name = "location")
-    private String location;
+    private String location = "";
 
     @Builder.Default
     @Column(name = "status", nullable = false)
@@ -71,6 +73,11 @@ public class Post extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member writer;
+
+    @Transient
+    private boolean following;
+    @Transient
+    private boolean liked;
 
     @ManyToMany
     @JoinTable(
@@ -107,5 +114,10 @@ public class Post extends BaseTimeEntity {
         this.privacyHeight = editPost.isPrivacyHeight();
         this.privacyWeight = editPost.isPrivacyWeight();
         return this;
+    }
+
+    public void updateIsFollowingAndIsLiked(boolean isFollowing, boolean isLiked) {
+        this.following = isFollowing;
+        this.liked = isLiked;
     }
 }
