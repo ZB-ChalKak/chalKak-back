fa526061f54238239ec1392e80bcad07cabc5ab0
myu7769 <myung7769@gamil.com>
2023-09-03 03:11:28 +0900

    [#104] fix : yml 파일 수정 및 comment 수정

--- src/main/java/com/btb/chalKak/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/btb/chalKak/domain/comment/repository/CommentRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
@@ -14,8 +13,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByPostId (Long postId);
 
     List<Comment> findCommentByPostId (Long postId);
-    @Query("DELETE FROM Comment c WHERE c.id = ?1")
-    int deleteCommentById (Long commentId);
+
+    void deleteById (Long commentId);
 
     Page<Comment> findByPostIdOrderByCreatedAtDesc (Long postId, Pageable pageable);
 
