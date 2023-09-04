7c67698ccce043451c8ea739a2b9f6fbbe6aea6b
jyjang <jinyngg@gmail.com>
2023-09-04 23:05:06 +0900

    [#130] feat : 작성 글 조회를 위한 Repo 메소드 추가

--- src/main/java/com/btb/chalKak/domain/post/repository/PostRepository.java
+++ src/main/java/com/btb/chalKak/domain/post/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.btb.chalKak.domain.post.repository;
 
+import com.btb.chalKak.domain.member.entity.Member;
 import com.btb.chalKak.domain.post.entity.Post;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PostRepository extends JpaRepository<Post, Long>, CustomPostRepository {
+
     List<Post> findAllByContentContaining(String keyword);
+
+    List<Post> findAllByWriter(Member writer);
 }
