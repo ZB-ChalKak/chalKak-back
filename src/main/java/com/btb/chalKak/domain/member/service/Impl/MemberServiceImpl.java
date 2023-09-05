cd5cfab1233274838dc145e228b4bc0decaeaeba
myu7769 <myung7769@gamil.com>
2023-09-05 18:09:49 +0900

    [#12] fix : build error 수정

--- src/main/java/com/btb/chalKak/domain/member/service/Impl/MemberServiceImpl.java
+++ src/main/java/com/btb/chalKak/domain/member/service/Impl/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package com.btb.chalKak.domain.member.service.Impl;
 
 import static com.btb.chalKak.common.exception.type.ErrorCode.*;
+
 import static com.btb.chalKak.domain.member.type.MemberProvider.CHALKAK;
 import static com.btb.chalKak.domain.member.type.MemberStatus.ACTIVE;
 import static com.btb.chalKak.domain.member.type.MemberStatus.BLOCKED;
