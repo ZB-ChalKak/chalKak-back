5d75996710450f3e11f23ef83b3bfa58318a3dce
jyjang <jinyngg@gmail.com>
2023-09-11 13:16:24 +0900

    [#176] refactor : QueryDsl 로직 점검
    1. 회원 태그 및 체형을 이용한 조회 제거
    2. 키워드 조회시 결과가 없을 경우 최신순 조회

--- src/main/java/com/btb/chalKak/domain/post/repository/CustomPostRepository.java
+++ src/main/java/com/btb/chalKak/domain/post/repository/CustomPostRepository.java
@@ -1,6 +1,5 @@
 package com.btb.chalKak.domain.post.repository;
 
-import com.btb.chalKak.domain.member.entity.Member;
 import com.btb.chalKak.domain.post.entity.Post;
 import java.util.List;
 import java.util.Optional;
@@ -14,12 +13,8 @@ public interface CustomPostRepository {
 
     Optional<Post> loadPublicPostDetails(Long postId);
 
-    Page<Post> loadPublicFeaturedPostsByMemberStyleTags(int page, int size, Member member);
-
-    Page<Post> loadPublicFeaturedPostsByMember(int page, int size, Member member);
-
     Page<Post> loadPublicFeaturedPostsByBodyTypeAndStyleTags(int page, int size, double height,
-            double weight, List<Long> styleTagIds, Member member);
+            double weight, List<Long> styleTagIds);
 
     Page<Post> loadLatestPublicPostsByMemberIds(List<Long> memberIds, int page, int size);
 
